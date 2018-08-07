ba8422abe795de46e39de6eaa554e54006bb6b97
NandiniNayak <nandini.r.nayak@gmail.com>
2018-08-08 06:35:40 +1000

    Update lesson6-loops.js

--- lesson6-loops.js
+++ lesson6-loops.js
@@ -1,11 +1,11 @@
 var arr = [3, 5, 7];
 arr.foo = 'hello';
 
-for (var i in arr) {
+for (let i in arr) {
    console.log(i); // logs "0", "1", "2", "foo"
 }
 
-for (var i of arr) {
+for (let i of arr) {
    console.log(i); // logs 3, 5, 7
 }
 
@@ -19,9 +19,9 @@ arrObj = [
   ["english","hindi","kannada","konkani"]
 ]
 
-arrObj.name = "test"; // valid syntax in array, adds the object to array but the value is not accessible
+arrObj.name = "test"; // valid syntax in array, adds the object property to an array but the value is not accessible
 console.log(arrObj.name); // test
-console.log(arrObj["name"]); // note: if an object is added to array, access it through its key not index
+console.log(arrObj["name"]); // note: if an object property is added to array, access it through its key not index
 console.log(arrObj[4]); //undefined as this is an object, access it through key
 
 for (var i in arrObj) {
@@ -31,4 +31,5 @@ for (var i in arrObj) {
 for (var i of arrObj) {
   console.log(i);
 }
-// note here the index will be udnefined for an object in the array
+// note here the index will be udnefined for an object property in the array
+
